3b8e6fc697349c8b68a7aa020875f771fa3ea160
Shahzod_Sharofiddinov <sharofiddinovshahzod3@gmail.com>
2023-07-19 20:52:34 +0500

    add model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Xizmatlar)
+admin.site.register(Doctors)
+admin.site.register(Contact)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Xizmatlar(models.Model):
+    title = models.CharField(max_length=25)
+    img = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=75)
+
+    def __str__(self):
+        return self.title    
+    
+class Doctors(models.Model):
+    img = models.ImageField(upload_to='images/')
+    name_doctor = models.CharField(max_length=25)
+    specialty = models.CharField(max_length=25)
+    instagram_link = models.URLField(max_length=150)
+    telegram_link = models.URLField(max_length=150)
+    facebook_link = models.URLField(max_length=150)
+
+    def __str__(self):
+        return f"{self.name_doctor} - {self.specialty}"
+    
+class Contact(models.Model):
+    user_name = models.CharField(max_length=25)
+    patient_name = models.CharField(max_length=30)
+    patient_about = models.CharField(max_length=255)
+    patient_number = models.PositiveIntegerField(max_length=12)
+
+    def __str__(self):
+        return f"{self.user_name} - {self.patient_name}"
+
+    
+
+
+
+    
+
